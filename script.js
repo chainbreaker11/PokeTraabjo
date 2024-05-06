0210186a661cd0a24b6a155b81e573606942bb73
chainbreaker11 <angelyahirruizmendoza@gmail.com>
2024-05-05 23:40:05 -0700

    correción de getPokemon(retornar null si el pokemon no existe)

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ const teamHistory = [];// Arreglo para el historial de equipos
 
 document.getElementById('add').addEventListener('click', async function () {
     if (pokemonArray.length >= 3) {
-        alert("¡El equipo ya tiene 6 Pokémon!");
+        alert("¡El equipo ya tiene 3 Pokémon!");
         return;
     }
     const pokemonName = document.getElementById('name').value;
-    const pokemonInfo = await getPokemon(pokemonName);
-    pokemonArray.push(pokemonInfo);
-    clearInput();
-    if (pokemonArray.length === 3) {
-        disableSearchAndAddButton();
+    try {
+        const pokemonInfo = await getPokemon(pokemonName);
+        if (pokemonInfo) {
+            pokemonArray.push(pokemonInfo);
+            clearInput();
+            if (pokemonArray.length === 3) {
+                disableSearchAndAddButton();
+            }
+        } else {
+            alert("¡No se encontró el Pokémon!");
+        }
+    } catch (error) {
+        alert(error.message);
     }
 });
 
@@ -23,30 +31,32 @@ document.getElementById('reset').addEventListener('click', function () {
 
 //botón historial
 document.getElementById('showHistory').addEventListener('click', function () {
-    showTeamHistory(); 
+    showTeamHistory();
 });
 
 async function getPokemon(name) {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
         if (!response.ok) {
+            if (response.status === 404) {
+                return null; // Retorna null si el Pokémon no existe
+            }
             throw new Error(`Error al buscar el Pokémon: ${response.status}`);
         }
         return response.json();
     } catch (error) {
-        alert(error.message);
-        console.log(error.message);
+        throw new Error(error.message);
     }
 }
 //funcion pa mostrar lso pokemones
 async function displayAllPokemon() {
-    const pokemonContainer = document.getElementById('pokemonContainer'); 
-    pokemonContainer.innerHTML = ''; 
+    const pokemonContainer = document.getElementById('pokemonContainer');
+    pokemonContainer.innerHTML = '';
 
     pokemonArray.forEach(pokemon => {
-        const pokemonDiv = document.createElement('div'); 
+        const pokemonDiv = document.createElement('div');
         pokemonDiv.classList.add('pokemon-card')
-        
+
         pokemon.types.forEach(type => {
             pokemonDiv.classList.add(type.type.name);
         });
@@ -59,10 +69,8 @@ async function displayAllPokemon() {
             <p>Ability: ${pokemon.abilities[0].ability.name}</p>
             <p>Base Experience: ${pokemon.base_experience}</p>
         `;
-        pokemonContainer.appendChild(pokemonDiv); 
-    });
-    
-    
+        pokemonContainer.appendChild(pokemonDiv);
+    })
 
     // Agregar el equipo actual al historial antes de limpiar el arreglo
     teamHistory.push([...pokemonArray]);
@@ -74,7 +82,7 @@ document.getElementById('send').addEventListener('click', function () {
 });
 
 function clearInput() {
-    document.getElementById('name').value = ''; 
+    document.getElementById('name').value = '';
 }
 
 // Función para deshabilitar la barra de búsqueda y el botón de agregar
@@ -101,11 +109,11 @@ let teamHistoryVisible = false; // Variable de estado para controlar la visibili
 
 function showTeamHistory() {
     const historyContainer = document.getElementById('historyContainer');
- 
+
     if (teamHistoryVisible) {
         historyContainer.innerHTML = '';
-        teamHistoryVisible = false; 
-        return; 
+        teamHistoryVisible = false;
+        return;
     }
 
     // Si el historial de equipos no está visible, lo mostramos
@@ -113,15 +121,15 @@ function showTeamHistory() {
     teamHistory.forEach((team, teamIndex) => {
         const teamDiv = document.createElement('div');
         teamDiv.classList.add('history-team'); // Agregar la clase para el estilo del equipo
-        
+
         team.forEach((pokemon, pokemonIndex) => {
             const pokemonCardDiv = document.createElement('div');
             pokemonCardDiv.classList.add('pokemon-card'); // Agregar la clase para el estilo de la carta del Pokémon
-            
+
             pokemon.types.forEach(type => {
-                pokemonCardDiv.classList.add(type.type.name); 
+                pokemonCardDiv.classList.add(type.type.name);
             });
-            
+
             pokemonCardDiv.innerHTML = `
                 <img src="${pokemon.sprites.front_shiny}" alt="${pokemon.name}">
                 <p>Name: ${pokemon.name}</p>
@@ -130,12 +138,12 @@ function showTeamHistory() {
                 <p>Ability: ${pokemon.abilities[0].ability.name}</p>
                 <p>Base Experience: ${pokemon.base_experience}</p>
             `;
-            
+
             teamDiv.appendChild(pokemonCardDiv);
         });
 
         historyContainer.appendChild(teamDiv);
     });
 
-    teamHistoryVisible = true; 
+    teamHistoryVisible = true;
 }
